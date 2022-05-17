c0ba4cdc554765be6c6416babf5e16d640c132b9
Asad Prodhan <52525544+asadprodhan@users.noreply.github.com>
2022-05-17 16:01:10 +0800

    Add files via upload

---
+++ script3_refseq_download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#textFormating
+Red="$(tput setaf 1)"
+Green="$(tput setaf 2)"
+reset=`tput sgr0` # turns off all atribute
+Bold=$(tput bold)
+#
+#FTP-links
+SAMPLES=*.csv
+#
+while IFS=, read -r field1 field2  
+
+do  
+    echo "${Red}${Bold} Downloading...${reset}: "${field1}""
+    echo "Name : $field1" 
+    echo "FTP-link : $field2" 
+        
+    wget "${field2}" -O ${field1}.fna.gz 
+    gzip -d ${field1}.fna.gz 
+    mv ${field1}.fna ${field1}.fasta
+    echo "${Green}${Bold} Download completed${reset}:"${field1}""
+    echo " "
+    
+done < ${SAMPLES}
+
